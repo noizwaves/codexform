b26bae7a4543933127cd89659b3a25c676f2e569
Adam Neumann <noizwaves@users.noreply.github.com>
2018-07-21 17:26:10 -0600

    Better name for variable

--- src/main/kotlin/io/noizwaves/codexform/application/ConsoleApplication.kt
+++ src/main/kotlin/io/noizwaves/codexform/application/ConsoleApplication.kt
@@ -6,7 +6,7 @@ import io.noizwaves.codexform.transformation.Transformation
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    val example = Transformation(listOf(
+    val lab5 = Transformation(listOf(
             // FleetTruck
             JavaReplaceMethodWithComment("io.pivotal.pal.wehaul.fleet.domain", "FleetTruck", "returnFromInspection", "Implement me"),
             JavaReplaceMethodWithReturnNull("io.pivotal.pal.wehaul.fleet.domain", "FleetTruck", "fleetDomainEvents"),
@@ -30,7 +30,7 @@ fun main(args: Array<String>) {
     }
 
     val targetPath = args[0]
-    example.apply(Paths.get(targetPath))
+    lab5.apply(Paths.get(targetPath))
 }
 
 fun showUsage() {
