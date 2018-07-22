cb04c9e41aa395bb71a200fb7fda0f14228807d3
Adam Neumann <noizwaves@users.noreply.github.com>
2018-07-22 17:56:13 -0600

    Improve usage text

--- src/main/kotlin/io/noizwaves/codexform/application/ConsoleApplication.kt
+++ src/main/kotlin/io/noizwaves/codexform/application/ConsoleApplication.kt
@@ -1,6 +1,5 @@
 package io.noizwaves.codexform.application
 
-import io.noizwaves.codexform.transformation.Transformation
 import java.nio.file.Paths
 import java.util.*
 
@@ -11,22 +10,18 @@ fun main(args: Array<String>) {
     }
 
     val enteredName = args[0]
-    val resolved = resolveDesiredTransformation(transformations, enteredName)
-    if (!resolved.isPresent) {
+    val desiredTransformation = Optional.ofNullable(transformations[enteredName])
+    if (!desiredTransformation.isPresent) {
         println("ERROR!! Unknown transformation '$enteredName'")
         showUsage()
         return
     }
 
-    val transformation = resolved.get()
+    val transformation = desiredTransformation.get()
     val transformDir = Paths.get(args[1])
     val workingDir2 = Paths.get(args[2])
 
     transformation.apply(transformDir, workingDir2)
-}
-
-fun resolveDesiredTransformation(choices: Map<String,Transformation>, name: String): Optional<Transformation> {
-    return Optional.ofNullable(choices[name])
 }
 
 private val transformations = hashMapOf(
@@ -34,7 +29,13 @@ private val transformations = hashMapOf(
         Pair("lab5start", lab5StartTransformations)
 )
 
-fun showUsage() {
-    println("""Usage: java -jar codexform.jar (transformation) (transform dir) (working dir)""")
+private fun showUsage() {
+    val names = transformations.keys.toList().sorted()
+
+    println("""
+Usage: java -jar codexform.jar (transformation) (transform dir) (working dir)
+
+Available transformations:""")
+    names.forEach { println("- $it") }
 }
 
