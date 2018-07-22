5f95c76b31502039b1e1f55bd3be3c0edf7698a9
Adam Neumann <noizwaves@users.noreply.github.com>
2018-07-22 17:44:44 -0600

    Switch transformation based on argument

--- src/main/kotlin/io/noizwaves/codexform/application/ConsoleApplication.kt
+++ src/main/kotlin/io/noizwaves/codexform/application/ConsoleApplication.kt
@@ -1,6 +1,8 @@
 package io.noizwaves.codexform.application
 
+import io.noizwaves.codexform.transformation.Transformation
 import java.nio.file.Paths
+import java.util.*
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
@@ -8,14 +10,31 @@ fun main(args: Array<String>) {
         return
     }
 
-    val transformation = lab5StartTransformations
-    val transformDir = args[0]
-    val workingDir = args[1]
+    val enteredName = args[0]
+    val resolved = resolveDesiredTransformation(transformations, enteredName)
+    if (!resolved.isPresent) {
+        println("ERROR!! Unknown transformation '$enteredName'")
+        showUsage()
+        return
+    }
 
-    transformation.apply(Paths.get(transformDir), Paths.get(workingDir))
+    val transformation = resolved.get()
+    val transformDir = Paths.get(args[1])
+    val workingDir2 = Paths.get(args[2])
+
+    transformation.apply(transformDir, workingDir2)
 }
 
+fun resolveDesiredTransformation(choices: Map<String,Transformation>, name: String): Optional<Transformation> {
+    return Optional.ofNullable(choices[name])
+}
+
+private val transformations = hashMapOf(
+        Pair("lab4start", lab4StartTransformations),
+        Pair("lab5start", lab5StartTransformations)
+)
+
 fun showUsage() {
-    println("""Usage: java -jar codexform.jar (transform dir) (working dir)""")
+    println("""Usage: java -jar codexform.jar (transformation) (transform dir) (working dir)""")
 }
 
